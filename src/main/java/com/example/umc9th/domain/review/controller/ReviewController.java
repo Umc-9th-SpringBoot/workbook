cae1eee3e1770801d1cc51ff8b9912b253e95b1a
rlawngjs0313 <juheon0313@naver.com>
2025-10-07 22:59:14 +0900

    ✨ feat: 6주차 워크북 제작용 API 설계 (Controller)

--- src/main/java/com/example/umc9th/domain/review/controller/ReviewController.java
+++ src/main/java/com/example/umc9th/domain/review/controller/ReviewController.java
@@ -1,4 +1,28 @@
 package com.example.umc9th.domain.review.controller;
 
+import com.example.umc9th.domain.review.entity.Review;
+import com.example.umc9th.domain.review.service.query.ReviewQueryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
 public class ReviewController {
+
+    private final ReviewQueryService reviewQueryService;
+
+    @GetMapping("/reviews/search")
+    public List<Review> searchReview(
+            @RequestParam String query,
+            @RequestParam String type
+    ){
+
+        // 서비스에게 요청
+        List<Review> result = reviewQueryService.searchReview(query, type);
+        return result;
+    }
 }
