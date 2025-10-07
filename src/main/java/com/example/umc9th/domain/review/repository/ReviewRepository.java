08e40b1fb1bb1a4a98adbde59f450e365c0fd905
rlawngjs0313 <juheon0313@naver.com>
2025-10-07 22:59:33 +0900

    ✨ feat: 6주차 워크북 제작용 API 설계 (Repository)

--- src/main/java/com/example/umc9th/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/example/umc9th/domain/review/repository/ReviewRepository.java
@@ -2,47 +2,6 @@ package com.example.umc9th.domain.review.repository;
 
 import com.example.umc9th.domain.review.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-
-    // 지역만 조회
-    @Query(
-            value = "SELECT r1.* " +
-                    "FROM review r1 " +
-                    "LEFT JOIN store s1 ON r1.store_id = s1.id " +
-                    "LEFT JOIN location l1 on s1.location_id = l1.id " +
-                    "WHERE l1.name LIKE CONCAT('%', :name, '%')", nativeQuery = true
-    )
-    List<Review> searchReviewByLocation(@Param("name") String name);
-
-    // 별점만 조회
-    @Query(
-            value = "SELECT r1.* " +
-                    "FROM review r1 " +
-                    "LEFT JOIN store s1 ON r1.store_id = s1.id " +
-                    "LEFT JOIN location l1 on s1.location_id = l1.id " +
-                    "WHERE r1.star > :star ", nativeQuery = true
-    )
-    List<Review> searchReviewByLocation(
-            @Param("star") Float star
-    );
-
-    // 지역 + 별점 조회
-    @Query(
-            value = "SELECT r1.* " +
-                    "FROM review r1 " +
-                    "LEFT JOIN store s1 ON r1.store_id = s1.id " +
-                    "LEFT JOIN location l1 on s1.location_id = l1.id " +
-                    "WHERE l1.name LIKE CONCAT('%', :name, '%') " +
-                    "AND r1.star > :star ", nativeQuery = true
-    )
-    List<Review> searchReviewByLocation(
-            @Param("name") String name,
-            @Param("star") Float star
-    );
-
+public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewQueryDsl {
 }
