6c288caf09c2ff69b5a6acc809b8c6107ff6268b
rlawngjs0313 <juheon0313@naver.com>
2025-10-07 22:59:21 +0900

    ✨ feat: 6주차 워크북 제작용 API 설계 (Service)

--- src/main/java/com/example/umc9th/domain/review/service/query/ReviewQueryService.java
+++ src/main/java/com/example/umc9th/domain/review/service/query/ReviewQueryService.java
@@ -1,4 +1,58 @@
 package com.example.umc9th.domain.review.service.query;
 
+import com.example.umc9th.domain.review.entity.QReview;
+import com.example.umc9th.domain.review.entity.Review;
+import com.example.umc9th.domain.review.repository.ReviewRepository;
+import com.example.umc9th.domain.store.entity.QLocation;
+import com.querydsl.core.BooleanBuilder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class ReviewQueryService {
+
+    private final ReviewRepository reviewRepository;
+
+    // 검색 API
+    public List<Review> searchReview(
+            String query,
+            String type
+    ){
+
+        // Q클래스 정의
+        QReview review = QReview.review;
+        QLocation location = QLocation.location;
+
+        // BooleanBuilder 정의
+        BooleanBuilder builder = new BooleanBuilder();
+
+        // BooleanBuilder 사용
+
+        // 동적 쿼리: 검색 조건
+        if (type.equals("location")) {
+            builder.and(location.name.contains(query));
+        }
+        if (type.equals("star")) {
+            builder.and(review.star.goe(Float.parseFloat(query)));
+        }
+        if (type.equals("both")) {
+
+            // & 기준 변환
+            String firstQuery = query.split("&")[0];
+            String secondQuery = query.split("&")[1];
+
+            // 동적 쿼리
+            builder.and(location.name.contains(firstQuery));
+            builder.and(review.star.goe(Float.parseFloat(secondQuery)));
+        }
+
+        // Repository 사용 & 결과 매핑
+        List<Review> reviewList = reviewRepository.searchReview(builder);
+
+        // 리턴
+        return reviewList;
+    }
 }
