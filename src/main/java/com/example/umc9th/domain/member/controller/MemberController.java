8a96835865c60e12801f12afc2c53bd0ceb61be9
rlawngjs0313 <juheon0313@naver.com>
2025-10-07 23:03:10 +0900

    ♻️ refactor: 불필요한 import문 제거

--- src/main/java/com/example/umc9th/domain/member/controller/MemberController.java
+++ src/main/java/com/example/umc9th/domain/member/controller/MemberController.java
@@ -1,14 +1,7 @@
 package com.example.umc9th.domain.member.controller;
 
-import com.example.umc9th.domain.member.service.query.MemberQueryService;
-import com.example.umc9th.domain.review.entity.Review;
-import com.example.umc9th.domain.review.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
