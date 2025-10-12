88761f48f98c1c669dc69ac437ba16308cfdc739
rlawngjs0313 <juheon0313@naver.com>
2025-10-13 00:18:51 +0900

    ✨ feat: Error Code 추가 (500 에러)

--- src/main/java/com/example/umc9th/global/apiPayload/code/GeneralErrorCode.java
+++ src/main/java/com/example/umc9th/global/apiPayload/code/GeneralErrorCode.java
@@ -9,17 +9,20 @@ import org.springframework.http.HttpStatus;
 public enum GeneralErrorCode implements BaseErrorCode{
 
     BAD_REQUEST(HttpStatus.BAD_REQUEST,
-            "COMMON400",
+            "COMMON400_1",
             "잘못된 요청입니다."),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED,
-            "AUTH401",
+            "AUTH401_1",
             "인증이 필요합니다."),
     FORBIDDEN(HttpStatus.FORBIDDEN,
-            "AUTH403",
+            "AUTH403_1",
             "요청이 거부되었습니다."),
     NOT_FOUND(HttpStatus.NOT_FOUND,
-            "COMMON404",
+            "COMMON404_1",
             "요청한 리소스를 찾을 수 없습니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,
+            "COMMON500_1",
+            "예기치 않은 서버 에러가 발생했습니다."),
     ;
 
     private final HttpStatus status;
