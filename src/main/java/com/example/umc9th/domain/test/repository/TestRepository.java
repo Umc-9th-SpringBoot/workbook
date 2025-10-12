f81ce2f3804de3f532e0a6bb0cb4c690935a2a6b
rlawngjs0313 <juheon0313@naver.com>
2025-10-12 22:05:37 +0900

    ✨ feat: Test를 위한 Controller, Service, Repository 생성

---
+++ src/main/java/com/example/umc9th/domain/test/repository/TestRepository.java
@@ -0,0 +1,4 @@
+package com.example.umc9th.domain.test.repository;
+
+public interface TestRepository {
+}
---
+++ src/main/java/com/example/umc9th/domain/test/service/query/TestQueryService.java
@@ -0,0 +1,4 @@
+package com.example.umc9th.domain.test.service.query;
+
+public class TestQueryService {
+}
